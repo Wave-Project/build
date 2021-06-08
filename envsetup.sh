4bda3271f8048f0de8bfb14e575d78b649053560
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-06-08 15:45:52 +0530

    envsetup: Remove duplicate ccache check

--- envsetup.sh
+++ envsetup.sh
@@ -1648,19 +1648,7 @@ addcompletions
 if [ -z ${CCACHE_EXEC} ]; then
     ccache_path=$(which ccache)
     if [ ! -z "$ccache_path" ]; then
-        export CCACHE_EXEC="$ccache_path"
-        echo "ccache found and CCACHE_EXEC has been set to : $ccache_path"
-    else
-        echo "ccache not found/installed!"
-    fi
-fi
-
-# check and set ccache path on envsetup
-if [ -z ${CCACHE_EXEC} ]; then
-    ccache_path=$(which ccache)
-    if [ ! -z "$ccache_path" ]; then
 	export USE_CCACHE=1
-	export CCACHE_COMPRESS=1
         export CCACHE_EXEC="$ccache_path"
 	echo -e "\e[1mccache enabled and \e[32m\e[4mCCACHE_EXEC\e[0m \e[1mhas been set to : \e[4m$ccache_path\e[0m"
     else
